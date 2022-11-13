5a81cfc02bdb1af0f30c1f26e5fb3f7427ed2889
Иван Провоторов <ivanpro2010@yandex.ru>
2022-11-13 17:58:57 +0300

    29

--- 29/Program.cs
+++ 29/Program.cs
@@ -2,3 +2,14 @@
 // 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
 // 6, 1, 33 -> [6, 1, 33]
 Console.Clear();
+int [] array = GetRandomArray(8,0,100);
+Console.WriteLine($"[{String.Join(", ", array)}]");
+int [] GetRandomArray(int size, int minValue, int maxValue)
+{
+    int [] result = new int [size];
+    for (int i = 0; i < size; i++)
+    {
+        result [i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return result;
+}
