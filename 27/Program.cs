2f7fc48e3d7c1d88033d83364ebf432b26f3aaf8
Иван Провоторов <ivanpro2010@yandex.ru>
2022-11-13 17:06:26 +0300

    27

--- 27/Program.cs
+++ 27/Program.cs
@@ -3,3 +3,17 @@
 // 82 -> 10
 // 9012 -> 12
 Console.Clear();
+Console.WriteLine("Программа принимает на ввод число и выдает сумму цифр в числе.");
+Console.Write("Задайте число: ");
+int num = int.Parse(Console.ReadLine());
+Console.WriteLine($"Сумма цифр = {GetSumNums(num)}");
+int GetSumNums(int number)
+{
+    int sum =0;
+    while(number > 0)
+    {
+        sum += number % 10;
+        number /= 10;
+    }
+    return sum;
+}
